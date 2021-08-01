236b3861f43702da7d236089ad15d64fc51d5cc8
Byron Varberg <Byron_Varberg@external.mckinsey.com>
2021-08-01 18:50:23 -0500

    Collapse logging groups

--- src/lib/console-analytics/ConsoleAnalytics.ts
+++ src/lib/console-analytics/ConsoleAnalytics.ts
@@ -2,6 +2,8 @@ import { Analytics } from "../../services/analytics";
 
 export class ConsoleAnalytics implements Analytics {
   async track(params: { eventName: string; payload?: unknown }): Promise<void> {
-    console.log(`[ANALYTICS] ${params.eventName}`, { payload: params.payload });
+    console.groupCollapsed(`[ANALYTICS] ${params.eventName}`);
+    console.log({ payload: params.payload });
+    console.groupEnd();
   }
 }
