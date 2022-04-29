596afaf22097cace3570b1275ea76d75b7801800
Byron Varberg <byronvarberg@gmail.com>
2022-04-28 21:47:02 -0500

    Add margin on buttons

--- src/packages/design-system/Button/Button.tsx
+++ src/packages/design-system/Button/Button.tsx
@@ -3,6 +3,7 @@ import styled from "styled-components/macro";
 export const Button = styled.button`
   font-size: 1em;
   padding: 0.25em 1em;
+  margin: 1em;
   border-radius: 3px;
   color: ${(props) => props.color ?? props.theme.colors.main};
   border: 2px solid ${(props) => props.color ?? props.theme.colors.main};
